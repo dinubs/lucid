07103008d1317fb5b6a3d430c8b64afabf298c72
Gavin Dinubilo <gavincodes@gmail.com>
2019-11-12 18:20:56 -0800

    More updates to the example page

--- example/maps/index.js
+++ example/maps/index.js
@@ -2,6 +2,6 @@ import BlockScriptingMap from './block_scripting';
 import BasicPlatformerMap from './basic_platformer';
 
 export default [
-  BlockScriptingMap,
   BasicPlatformerMap,
+  BlockScriptingMap,
 ];
